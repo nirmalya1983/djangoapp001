7021c652193d768c4e3c7a1fdfe127c9f6f6c6ea
Nirmalya <nirmalya1983@gmail.com>
2020-04-11 18:54:44 +0530

    commit requirements

--- testProject001/urls.py
+++ testProject001/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('covt19.urls')),
+    path(r'^', include('covt19.urls')),
  
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
