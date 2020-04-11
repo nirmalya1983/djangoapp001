5f66cf3cd85370910feeb5319a927ed6786da193
nirmalya1983 <30698561+nirmalya1983@users.noreply.github.com>
2020-04-11 08:58:11 +0530

    Add files via upload

---
+++ static/js/nirmalya.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myObj = JSON.parse(this.responseText);
+        //console.log(myObj);
+		populateVueData(myObj,stopOnloading)
+    }
+    if (this.readyState == 4 && this.status != 200){
+    	console.log("error in getting data:"+str(this.status))
+    }
+	};
+	xmlhttp.open("GET", "/static/js/result.js", true);
+	xmlhttp.send();
+})
+function populateVueData(datain,callback){
+	//console.log("here")
+	var example1 = new Vue({
+		el: '#data1',
+		data: {
+		items: datain
+		}
+	})
+	setTimeout(callback,10)
+}
+function stopOnloading(){
+	$("#onLoading").addClass("d-none");
+	$("#BaseCont").removeClass("d-none");
+	$("body").addClass("loadnormal");
+	$("body").removeClass("loadbg");
+}
+
