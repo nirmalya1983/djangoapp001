b6fc3555ce25b2c27b2938b4057571a9cf495887
Nirmalya <nirmalya1983@gmail.com>
2020-04-11 18:34:08 +0530

    commit requirements

--- testProject001/settings.py
+++ testProject001/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'kn9(48c6+5m!tsk6uzpt^84^=h_j%oxb0fbyu^&k(9434#9dgb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nirmalyaapp1.herokuapp.com']
 
 
 # Application definition
